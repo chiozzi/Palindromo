9559d41c0b2eede9fb9dcb8f422462c7b0b56945
chiozzi <chiozziletici0114@gmail.com>
2025-03-10 09:10:08 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Palindromo';
+  inputText: string = '';
+  verifica: boolean = false;
+
+  verificarPalindromo(): void {
+    //remove espaços em branco e converte o texto para minúsculas
+    const texto = this.inputText.replace(/\s+/g, '').toLowerCase();
+    
+    //inverte o texto
+    const textoInvertido = texto.split('').reverse().join('');
+    
+    //compara o texto original com o texto invertido
+    this.verifica = texto === textoInvertido;
+  }
+  
 }
